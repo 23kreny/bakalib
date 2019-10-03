7a7eabfca3bde4f9c06832cad1056208fc65c1a7
Martin Kröner <kronerm9@gmail.com>
2019-10-03 17:18:32 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
 
 setuptools.setup(
     name="bakalib",
-    version="0.3.6",
+    version="0.3.7",
     author="kreny",
     author_email="kronerm9@gmail.com",
     description="A library for accessing the module data of Bakaláři school system easily",
