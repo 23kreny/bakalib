93f20b2ebf1ee424ccfa7ecee388a9a118f6ce37
Martin Kröner <kronerm9@gmail.com>
2019-12-20 11:16:10 +0100

    Major code refactor.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-import setuptools
+from setuptools import find_packages, setup
+
+from bakalib._version import version
 
 with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
-setuptools.setup(
+setup(
     name="bakalib",
-    version="0.6.2",
+    version=version,
     author="kreny",
     author_email="kronerm9@gmail.com",
     description="A library for accessing the module data of Bakaláři school system easily",
@@ -13,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/23kreny/bakalib",
     include_package_data=True,
-    packages=["bakalib"],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
